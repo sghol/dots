596e1c46439198809c9c3a42a3f887f5b4588533
Saeed Gholami <saeed.ghollami@gmail.com>
2026-04-13 14:23:42 +0330

    Minor update.

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,10 +54,6 @@ vim.opt.splitright = true
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
 vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#FCE094", fg = "#07080D" })
-vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#000000" })
 
 -- ----------------------
 -- COMMANDS / FUNCTIONS
